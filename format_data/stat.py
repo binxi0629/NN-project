b98bf26d05f05b9a6094ca06882c09355aa5efb5
Bin XI <binxi_@outlook.com>
2019-06-08 20:07:26 +0800

    Add scirpt to stat some info

--- format_data/stat.py
+++ format_data/stat.py
@@ -2,6 +2,9 @@ import format_data
 import os
 import json
 import numpy as np
+from collections import Counter
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def statistic():
@@ -33,6 +36,43 @@ def statistic():
         json.dump(stat_res, f, cls=format_data.NumpyEncoder, indent=4)
 
 
+def stat_bands():
+    with open("stat_res.json", 'r') as f:
+        data = json.load(f)
+
+    save_plot_data = {}
+    save_plot_data["bands"] = []
+
+    for i in data:
+        save_plot_data["bands"].append(data[i]["bands"]["total_bands"])
+
+    with open('plot_data.json', 'w') as f:
+        json.dump(save_plot_data, f, cls=format_data.NumpyEncoder, indent=2)
+
+
+def plot_bands():
+    with open("plot_data.json", 'r') as f:
+        bands_info = json.load(f)
+
+    tmp = bands_info["bands"]
+
+    res = Counter(tmp)
+    new_plot = {}
+    new_plot["bands_info"] =[]
+    for x in res:
+        new_plot["bands_info"].append({"bands": x, "weights": res[x]})
+
+    df = pd.DataFrame(new_plot["bands_info"])
+    print(df)
+    # df.plot()
+    plot = df.plot.bar(x='weights', y='bands')
+    # fig = plot.get_figure()
+    # fig.savefig('bands_info.png')
+    # plt.show()
+    # with open('bands_res.json', 'w') as f:
+    #    json.dump(res, f, cls=format_data.NumpyEncoder, indent=2)
+
+
 class NumpyEncoder(json.JSONEncoder):
     """
         to solve Error: NumPy array is not JSON serializable
@@ -45,4 +85,7 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 if __name__ == '__main__':
-    statistic()
+    # statistic()
+    plt.close("all")
+    plot_bands()
+
