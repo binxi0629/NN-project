16f49c347e448043d6f4b3b91ac9e20bf9c040e1
Bin XI <binxi_@outlook.com>
2019-06-10 17:24:23 +0800

    os.walk replaces original for loop

--- format_data/load_data.py
+++ format_data/load_data.py
@@ -2,8 +2,10 @@ from pymatgen.ext.matproj import MPRester
 from pathlib import Path
 import format_data
 import json
+import os
+import numpy as np
 
-m = MPRester("Hyxf8a7HI7RhXZI1kaFT")
+m = MPRester("HvxfB98DFTCPiadV")
 
 
 def get_space_group(file_id):  # function to get space group by id
@@ -13,25 +15,70 @@ def get_space_group(file_id):  # function to get space group by id
     return doc["spacegroup"]["symbol"], doc["spacegroup"]["number"]
 
 
-def main():
-    print('running..., please wait')
+def create_data():
+    data = {}
+    data_dir = '../data/'
+    data["number"] = 0
+    data["spacegroup"] = None
+    count = 1
 
-    for file_id in range(10):
-        file = Path("../data/raw_data_{}.json".format(file_id))
-        if file.is_file():
-            print("File raw_data_{}.json exists, formatting file...       TOTAL: {}/10,000".format(file_id, file_id))
+    for subdir, dirs, files in os.walk(data_dir):
+        num_files = len(files)
+        for i in range(num_files):
+            with open(data_dir + files[i]) as f:
+                data_json = json.load(f)
 
-            # step 0: initialize
+                mp_id = data_json["id"]
+                data['id'] = mp_id
+                this_data = format_data.BandsData(mp_id=mp_id)
+                bands = data_json["band"]["bands"]
+                bands = np.array(bands)
+                branches = data_json["band"]["branches"]
+                formatted_bands, new_dict = this_data.format_data(bands, branches)
+                degen_bands = this_data.degen_translate(formatted_bands)
+                new_bands = this_data.fix_bands_dim(degen_bands)
+                data["bands"] = new_bands
+                try:
+                    data["spacegroup"], data["number"] = get_space_group(mp_id)
+                except:
+                    print("{} is not found".format(mp_id))
+                    continue
+
+                save_file_name = 'input_data_{}.json'.format(mp_id)
+                with open('../input_data/{}'.format(save_file_name), 'w') as file:
+                    json.dump(data, file, cls=format_data.NumpyEncoder, indent=4)
+                    print("finished... {}/21,738".format(count))
+                    count += 1
+
+
+def add_data():
+    """
+        Fixed the issue: https://github.com/binxi0629/NN-project/issues/4
+    :return:
+    """
+
+    with open('error_files.json', 'r') as f:
+        files_id = json.load(f)
+        for i in files_id["mp_id"]:
+            try:
+                get_space_group(i)
+            except:
+                print("{} is not found".format(i))
+                continue
+
+            sg, sg_number = get_space_group(i)
+
+
             data = {}
             data["bands"] = []
             data["number"] = 0
             data["spacegroup"] = None
 
             # step 1: new a format_data cls
-            this_data = format_data.BandsData(mp_id=file_id)
+            this_data = format_data.BandsData(mp_id=i)
 
             # step 2: load the bands matrix by mp_id
-            bands, branches = this_data.load_data(mp_id=file_id)
+            bands, branches = this_data.load_data(mp_id=i)
 
             # step 3: format bands to a __gen_dict form
             formatted_bands, new_dict = this_data.format_data(bands, branches)
@@ -45,21 +92,19 @@ def main():
             # step 6: save in a dict
             data["bands"] = fixed_bands
 
-            data["spacegroup"], data["number"] = get_space_group(file_id)
+            data["spacegroup"], data["number"] = sg, sg_number
 
             # need one more step to store data["sg_label"]
 
             # save in a .json file
-            save_file_name = 'input_data_{}.json'.format(file_id)
+            save_file_name = 'input_data_{}.json'.format(i)
 
-            with open('../input_data/{}'.format(save_file_name), 'w') as f:
-                json.dump(data, f, cls=format_data.NumpyEncoder, indent=4)
-            print("File raw_data_{}.json saved, loading next file...      TOTAL: {}/10,000".format(file_id, file_id))
-        else:
-            print("File raw_data_{}.json NOT FOUND, loading next file...  TOTAL: {}/10,000".format(file_id, file_id))
+            with open('../input_data/{}'.format(save_file_name), 'w') as f2:
+                json.dump(data, f2, cls=format_data.NumpyEncoder, indent=4)
 
-    print('All files successfully saved, please check in /input_data/')
+            print("File raw_data_{}.json saved, loading next file... ".format(i))
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    create_data()
